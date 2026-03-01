18b8d83d51522887849ddeb6439718d9b08e9cdd
JoseJVS <villamar_jose@hotmail.com>
2026-03-01 12:10:12 +0100

    Working sapi test with local connection assertion

--- python/tests/sapi_tests/sapi_test.py
+++ python/tests/sapi_tests/sapi_test.py
@@ -1,9 +1,5 @@
 import logging
-import sys
 from argparse import ArgumentParser
-from functools import reduce
-from hashlib import sha256
-from pathlib import Path
 from traceback import format_exc
 
 LOG = logging.getLogger(__name__)
@@ -101,7 +97,11 @@ def main() -> None:
             "allow_multiplicity": False,
             "conn_gen_name": "PairWiseBernoulli",
             "weight_df_name": "Distance",
-            "delay_df_name": "Constant",
+            "weight_ufs_names": ["LowerBound", "Inverse"],
+            "weight_ufs_params": [[0.0001, 0.0001], []],
+            "delay_df_name": "Distance",
+            "delay_ufs_names": ["Offset"],
+            "delay_ufs_params": [[0.1]],
             "prob_df_name": "Constant" if args.fixed_conn else "Distance",
             "prob_df_params": [args.conn_chance] if args.fixed_conn else [],
             "prob_ufs_names": [] if args.fixed_conn else ["Exponential"],
@@ -111,6 +111,52 @@ def main() -> None:
 
     nestgpu.Calibrate()
 
+    if sp_ns.local_index is not None and sp_ns.local_length is not None:
+        ns = nestgpu.NodeSeq(sp_ns.local_index, sp_ns.local_length)
+        conn_list = nestgpu.GetConnections(ns, ns)
+        conn_status = nestgpu.GetConnectionStatus(conn_list)
+
+        gpu_conn_map = {}
+        for conn_d in conn_status:
+            source = conn_d["source"]
+            target = conn_d["target"]
+            weight = conn_d["weight"]
+            delay = conn_d["delay"]
+            if source in gpu_conn_map:
+                source_map = gpu_conn_map[source]
+                assert target not in source_map
+                source_map[target] = (weight, delay)
+            else:
+                source_map = gpu_conn_map[source] = {}
+                source_map[target] = (weight, delay)
+
+        spatial_conns = nestgpu.get_spatial_connections(conn_index)
+
+        spatial_conn_map = {}
+        for conn_t in spatial_conns[1][local_rank]:
+            source, target, weight, delay = conn_t
+            if source in spatial_conn_map:
+                source_map = spatial_conn_map[source]
+                assert target not in source_map
+                source_map[target] = (weight, delay)
+            else:
+                source_map = spatial_conn_map[source] = {}
+                source_map[target] = (weight, delay)
+
+        assert len(gpu_conn_map) == len(spatial_conn_map)
+
+        for source, target_map in gpu_conn_map.items():
+            assert source in spatial_conn_map
+            sp_target_map = spatial_conn_map[source]
+            assert len(target_map) == len(sp_target_map)
+            for target, (weight, delay) in target_map.items():
+                assert target in sp_target_map
+                sp_weight, sp_delay = sp_target_map[target]
+                computed_sp_delay = float(
+                    int(np.round(np.float32(sp_delay) / np.float32(0.1)))
+                    * np.float32(0.1)
+                )
+                assert weight == sp_weight and np.isclose(delay, computed_sp_delay)
 
 if __name__ == "__main__":
     try:
