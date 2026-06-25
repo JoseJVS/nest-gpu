4594239016541036e8997bd9e0da6d9e442fc961
JoseJVS <villamar_jose@hotmail.com>
2026-06-25 14:38:20 +0200

    WIP: CMake refactor pt 5

--- cmake/CheckIncludesSymbols.cmake
+++ cmake/CheckIncludesSymbols.cmake
@@ -18,8 +18,8 @@
 # along with NEST GPU.  If not, see <http://www.gnu.org/licenses/>.
 
 # Here we check for all required include headers, types, symbols and functions.
-find_package( CUDAToolkit REQUIRED )
-find_library( MATH_LIBRARY m REQUIRED )
+find_package( CUDAToolkit REQUIRED COMPONENTS CXX )
+find_library( MATH_LIBRARY m REQUIRED COMPONENTS CXX )
 
 
 # Localize the Python interpreter and ABI
